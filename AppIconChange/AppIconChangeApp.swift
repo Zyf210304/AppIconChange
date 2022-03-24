d5d0264347b96702f75a1cb2a20d5de87c2ce7a6
张亚飞 <672233580@qq.com>
2022-03-24 22:26:14 +0800

    Initial Commit

---
+++ AppIconChange/AppIconChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIconChangeApp.swift
+//  AppIconChange
+//
+//  Created by 亚飞 on 2022/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AppIconChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppIconChange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppIconChange
+//
+//  Created by 亚飞 on 2022/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
